6b1755018028d127a8b1ea6c6d5a29dc56ee0954
Didy <davidalexanderaltamirano@gmail.com>
2026-02-22 22:03:14 -0600

    elimina carpetas lib y test dispersas

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-
-void main() => runApp(
-  const MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()),
-);
